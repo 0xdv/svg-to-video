f001c40b5aabcea77d6f4ccab3032430ec3a998e
coffeecode <35342238+0xC0FFEEC0DE@users.noreply.github.com>
2019-11-02 18:45:16 +0200

    added controls

--- video.js
+++ video.js
@@ -2,18 +2,28 @@ export default config => {
     let {
         drawScene,
         width,
-        height
+        height,
+        fps = 60,
+        bitrate = 50000000,
+        videoFormat = "video/webm",
+        outputContainer = document.querySelector('body'),
+        autoplay = true,
     } = config
 
     let canvas = document.createElement("canvas")
     canvas.width = width
     canvas.height = height
 
+    let div = document.createElement('div')
+    div.id = "videoContainer"
+
+    outputContainer.appendChild(div)
+
     let context = canvas.getContext("2d")
 
     let data = [],
         stream = canvas.captureStream(),
-        recorder = new MediaRecorder(stream, { mimeType: "video/webm", bitsPerSecond: 50000000 });
+        recorder = new MediaRecorder(stream, { mimeType: videoFormat, bitsPerSecond: bitrate });
 
     recorder.ondataavailable = function(event) {
         if (event.data && event.data.size) {
@@ -22,42 +32,86 @@ export default config => {
     }
 
     recorder.onstop = () => {
-        var url = URL.createObjectURL(new Blob(data, { type: "video/webm" }));
-        d3.select("body")
-            .append("video")
-            .attr("src", url)
-            .attr("controls", true)
-            .attr("autoplay", true);
+        let url = URL.createObjectURL(new Blob(data, { type: videoFormat }));
+
+        div.innerHTML = ''
+
+        let video = document.createElement('video')
+        video.src = url
+        video.controls = true
+        video.autoplay = autoplay
+        div.appendChild(video)
     }
 
-    function record() {
-        var queue = d3.queue(1)
+    let timer = null
+    let queue = null
+    let frame = 0
 
-        let frameCount = 120
+    function init() {
+        console.log('init')
+        let body = document.querySelector('body')
 
-        d3.range(frameCount).forEach(function(frame){
-            queue.defer(drawImg, frame / frameCount)
-        })
+        let panel = document.createElement('div')
+        panel.style.position = 'fixed'
+        panel.style.right = '4px'
+        panel.style.bottom = 0
+        panel.style['background-color'] = '#21212121'
+        panel.style['z-index'] = 42
+        panel.style.margin = '5px'
+        body.appendChild(panel)
 
-        queue.awaitAll(function(err, frames) {
-            if(err) throw err
+        let status = document.createElement('span')
+        status.textContent = 'Ready'
+        status.style.margin = '5px'
+        panel.appendChild(status)
 
-            recorder.start()
+        let recordBtn = document.createElement('button')
+        recordBtn.textContent = 'Record'
+        recordBtn.style.margin = '5px'
+        panel.appendChild(recordBtn)
 
-            ;(function processFrame() {
-                if (frames.length) {
-                    context.drawImage(frames.shift(), 0, 0, width, height);
-                    requestAnimationFrame(processFrame)
-                } else {
-                    recorder.stop()
-                }
-            })()
+        recordBtn.onclick = function(e) {
+            status.textContent = 'Recording...'
+            timer = setInterval(recordOne, 1000 / fps)
+            queue = d3.queue(1)
+        }
+        
 
-        })
+        let stopBtn = document.createElement('button')
+        stopBtn.textContent = 'Stop'
+        stopBtn.style.margin = '5px'
+        panel.appendChild(stopBtn)
+
+        stopBtn.onclick = function(e) {
+            status.textContent = 'Rendering...'
+            clearInterval(timer)
+            console.log("frames = ", frame)
+            queue.awaitAll(function(err, frames) {
+                if(err) throw err
+    
+                recorder.start()
+    
+                ;(function processFrame() {
+                    if (frames.length) {
+                        context.drawImage(frames.shift(), 0, 0, width, height);
+                        requestAnimationFrame(processFrame)
+                    } else {
+                        recorder.stop()
+                        status.textContent = 'Done'
+                    }
+                })()
+    
+            })
+        }
+        
+
     }
 
+    init()
 
-    let i = 0
+    function recordOne() {
+        queue.defer(drawImg, frame++)
+    }
 
     function drawImg(t, cb) {
         let domEl = drawScene(t)
@@ -65,29 +119,11 @@ export default config => {
         let serialized = new XMLSerializer().serializeToString(domEl)
         let blob = new Blob([serialized], {type: "image/svg+xml"})
         let url = URL.createObjectURL(blob)
-        let img = new Image()
-       // download(`tmp/${i++}.svg`, [serialized])
 
+        let img = new Image()
         img.src = url
         img.onload = function() {
             cb(null, img)
         }
     }
-
-    function download(filename, text) {
-        var pom = document.createElement('a');
-        pom.setAttribute('href', 'data:image/svg+xml,' + encodeURIComponent(text));
-        pom.setAttribute('download', filename);
-    
-        if (document.createEvent) {
-            var event = document.createEvent('MouseEvents');
-            event.initEvent('click', true, true);
-            pom.dispatchEvent(event);
-        }
-        else {
-            pom.click();
-        }
-    }
-
-    return record
 }
