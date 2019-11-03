5d01e398438faa569e000631e7d1266f6ba598d7
coffeecode <35342238+0xC0FFEEC0DE@users.noreply.github.com>
2019-11-03 21:50:52 +0200

    refactor

--- video.js
+++ video.js
@@ -10,14 +10,14 @@ export default config => {
         autoplay = true,
     } = config
 
-    let canvas = document.createElement("canvas")
-    canvas.width = width
-    canvas.height = height
-
     let div = document.createElement('div')
     div.id = "videoContainer"
 
     outputContainer.appendChild(div)
+
+    let canvas = document.createElement("canvas")
+    canvas.width = width
+    canvas.height = height
 
     let context = canvas.getContext("2d")
 
@@ -47,8 +47,9 @@ export default config => {
     let queue = null
     let frame = 0
 
-    function init() {
-        console.log('init')
+    let status, recordBtn, stopBtn
+
+    function menu() {
         let body = document.querySelector('body')
 
         let panel = document.createElement('div')
@@ -60,57 +61,21 @@ export default config => {
         panel.style.margin = '5px'
         body.appendChild(panel)
 
-        let status = document.createElement('span')
+        status = document.createElement('span')
         status.textContent = 'Ready'
         status.style.margin = '5px'
         panel.appendChild(status)
 
-        let recordBtn = document.createElement('button')
+        recordBtn = document.createElement('button')
         recordBtn.textContent = 'Record'
         recordBtn.style.margin = '5px'
         panel.appendChild(recordBtn)
 
-        recordBtn.onclick = function(e) {
-            status.textContent = 'Recording...'
-            timer = setInterval(recordOne, 1000 / fps)
-            queue = d3.queue(1)
-        }
-        
-
-        let stopBtn = document.createElement('button')
+        stopBtn = document.createElement('button')
         stopBtn.textContent = 'Stop'
         stopBtn.style.margin = '5px'
+        stopBtn.disabled = true
         panel.appendChild(stopBtn)
-
-        stopBtn.onclick = function(e) {
-            status.textContent = 'Rendering...'
-            clearInterval(timer)
-            console.log("frames = ", frame)
-            queue.awaitAll(function(err, frames) {
-                if(err) throw err
-    
-                recorder.start()
-    
-                ;(function processFrame() {
-                    if (frames.length) {
-                        context.drawImage(frames.shift(), 0, 0, width, height);
-                        requestAnimationFrame(processFrame)
-                    } else {
-                        recorder.stop()
-                        status.textContent = 'Done'
-                    }
-                })()
-    
-            })
-        }
-        
-
-    }
-
-    init()
-
-    function recordOne() {
-        queue.defer(drawImg, frame++)
     }
 
     function drawImg(t, cb) {
@@ -126,4 +91,51 @@ export default config => {
             cb(null, img)
         }
     }
+
+    function init() {
+        console.log('init')
+        menu()
+
+        recordBtn.onclick = function(e) {
+            status.textContent = 'Recording...'
+            stopBtn.disabled = false
+            this.disabled = true
+            frame = 0
+
+            timer = setInterval(() => {
+                queue.defer(drawImg, frame++)
+            }, 1000 / fps)
+            queue = d3.queue(1)
+        }
+
+        stopBtn.onclick = function(e) {
+            status.textContent = 'Rendering...'
+            this.disabled = true
+
+            clearInterval(timer)
+            console.log("frames = ", frame)
+
+            queue.awaitAll(function(err, frames) {
+                if(err) throw err
+    
+                recorder.start()
+    
+                ;(function processFrame() {
+                    if (frames.length) {
+                        context.drawImage(frames.shift(), 0, 0, width, height);
+                        requestAnimationFrame(processFrame)
+                    } else {
+                        recorder.stop()
+                        status.textContent = 'Done'
+                        recordBtn.disabled = false
+                    }
+                })()
+    
+            })
+        }
+        
+
+    }
+
+    init()
 }
